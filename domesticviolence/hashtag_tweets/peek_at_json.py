b2fa8d22185d6f8c0274b69c465ea3bbd62d3e96
Charley Lewittes <ctlewitt@gmail.com>
2016-09-14 17:58:45 -0400

    prints a sample of a data file, since they are too large to open or read in entirety; good for checking if a filter removed too much or too little

---
+++ domesticviolence/hashtag_tweets/peek_at_json.py
@@ -0,0 +1,67 @@
+from domesticviolence.util import (get_tweet_parts, MEDIA_PROVIDER, HEADLINE, AUTHOR, CONTENT, ARTICLE_URL, MEDIA_PROVIDER, PUBLISH_DATE,
+                  FOLLOWING, FOLLOWERS, UPDATES, BLOG_POST_SENTIMENT, ARTICLE_ID)
+
+
+
+'''line_num = 0
+with open("WhyIStayed_WhyILeft.json", "r") as read_file:
+    with open("miniWISWILjson.json", "w") as write_file:
+        for line in read_file:
+            line_num+=1
+            if line_num >5000 and line_num <5500:
+                write_file.write(line)
+'''
+"""
+line_num = 0
+with open("WhyIStayed_WhyILeft.csv", "r") as read_file:
+    with open("miniWISWILjson.csv", "w") as write_file:
+        while line_num < 500:
+            line_num+=1
+            line = read_file.readline()
+            write_file.write(line)
+
+"""
+"""
+line_num = 0
+with open("WhyIStayed_WhyILeft.csv", "r") as read_file:
+    while line_num < 500:
+        line_num+=1
+        print(read_file.readline())
+"""
+"""
+line_num = 0
+with open("WhyIStayed_WhyILeft.csv", "r") as read_file:
+    for line in read_file:
+        line_num += 1
+print(line_num)
+"""
+
+
+# prints an evenly spaced sample of 100 lines from a document
+def print_sample(filename, part=None):
+    line_num = 0
+    with open(filename, "r") as read_file:
+        tweet_part_idxes = [HEADLINE, AUTHOR, CONTENT, ARTICLE_URL, MEDIA_PROVIDER, PUBLISH_DATE, FOLLOWING, FOLLOWERS,
+                            UPDATES, BLOG_POST_SENTIMENT, ARTICLE_ID]
+        words_for_tweet_parts = ["HEADLINE", "AUTHOR", "CONTENT", "ARTICLE_URL", "MEDIA_PROVIDER", "PUBLISH_DATE",
+                                 "FOLLOWING", "FOLLOWERS", "UPDATES", "BLOG_POST_SENTIMENT", "ARTICLE_ID"]
+        for line in read_file:
+            line_num += 1
+    with open(filename, "r") as read_file:
+        frequency = line_num//100
+        line_num = 0
+        if part:
+            tweet_part_idx = words_for_tweet_parts.index(part)
+        for line in read_file:
+            line_num += 1
+            if line_num == frequency:
+                tweet_parts = get_tweet_parts(line)
+                if part:
+                    print("{}: {}".format(part, tweet_parts[tweet_part_idx]))
+                else:
+                    for tweet_part_idx in tweet_part_idxes:
+                        print(words_for_tweet_parts[tweet_part_idx], ": ", tweet_parts[tweet_part_idx])
+                line_num = 0
+                print(line)
+
+print_sample("WISWIL_filtered_twitter_not.txt", "MEDIA_PROVIDER")
